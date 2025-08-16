661cb908568956d2277b55053d6c493a6cf2f4a5
SeongYong_Song <songsy0612@gmail.com>
2025-08-16 18:46:18 +0900

    mainView json 연동 전

---
+++ camplate_ios/camplate_ios/jsonLoader.swift
@@ -0,0 +1,7 @@
+//
+//  Untitled.swift
+//  Camplate_iOS
+//
+//  Created by SeongYongSong on 8/16/25.
+//
+
---
+++ camplate_ios/camplate_ios/mainViewTest.swift
@@ -0,0 +1,18 @@
+//
+//  mainViewTest.swift
+//  Camplate_iOS
+//
+//  Created by SeongYongSong on 8/16/25.
+//
+
+import SwiftUI
+
+struct mainViewTest: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    mainViewTest()
+}
